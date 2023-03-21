5cde164191b830ee4da7664e6367e08cd291fde9
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-03-21 18:20:47 -0300

    Aula 210323

--- Aulas React/aula 200323/src/components/Porcentagem/Porcentagem.jsx
+++ Aulas React/aula 200323/src/components/Porcentagem/Porcentagem.jsx
@@ -15,7 +15,7 @@ export function Porcentagem() {
   return (
     <div className="porcentagem">
       <input type="number" placeholder="valor" onChange={(e) => setValor(e.target.value)} value={valor} />
-      <input type="number" placeholder="percentual" onChange={(e) => setPercentual(e.target.value)} value={percentual} />
+      <input type="number" placeholder="percentual" max={100} onChange={(e) => setPercentual(e.target.value)} value={percentual} />
       <br />
       {resultado !== 0 && <h3>Resultado: {resultado.toFixed(2)}</h3>}
     </div>
